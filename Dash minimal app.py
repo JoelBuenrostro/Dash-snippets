82a3779b3e30b4559c5f1ad25dda76fae0e58116
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 10:29:00 -0500

    Update Dash minimal app.py
    Add some comments

--- Dash minimal app.py
+++ Dash minimal app.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
+#Dash imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+#External css style sheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#Main app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+#App layout, it describes what the applications looks like
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -28,5 +32,6 @@ app.layout = html.Div(children=[
     )
 ])
 
+#App server
 if __name__ == '__main__':
     app.run_server(debug=True)
