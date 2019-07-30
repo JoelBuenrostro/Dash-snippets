1e7c912bedb0b5288b5136fc3fcd12a5da1212ff
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 15:03:51 -0500

    Create Text Input.py
    Text input snippet

---
+++ Layout snippets/Text Input.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Label('Text Input'),
+    dcc.Input(value='MTL', type='text')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
