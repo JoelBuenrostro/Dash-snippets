1f69fc14b003d756565b5cd73f809f854ecc7895
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 16:03:19 -0500

    Create Range slider.py
    Range slider and comments added

--- Layout snippets/Range slider.py
+++ Layout snippets/Range slider.py
@@ -12,13 +12,13 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 #App layout, it describes what the applications looks like
 app.layout = html.Div([
-    html.Label('Slider'),
-    dcc.Slider(
-        min=0,
-        max=9,
-        marks={i: 'Label {}'.format(i) for i in range(10)},
-        value=5,
-    )
+    dcc.RangeSlider(
+        count=1,
+        min=-5,
+        max=10,
+        step=0.5,
+        value=[-3, 7]
+    ) 
 ])
 
 #App server
