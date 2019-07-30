5c1d3457be5e54eb938244d72198e63a66aecb32
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 10:37:54 -0500

    Create Bar graph.py
    Simple bar graph snippet

---
+++ Bar graph.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#Dash imports
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#External css style sheet
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#Main app
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#App layout, it describes what the applications looks like
+app.layout = html.Div(children=[
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+#App server
+if __name__ == '__main__':
+    app.run_server(debug=True)
