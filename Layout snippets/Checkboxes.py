65b1119f510db1eff11fe95103f866d953ae28e8
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 16:15:37 -0500

    Update Checkboxes.py
    Add comments

--- Layout snippets/Checkboxes.py
+++ Layout snippets/Checkboxes.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
+#Dash imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+#External css style sheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#Main app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+#App layout, it describes what the applications looks like
 app.layout = html.Div([
     html.Label('Checkboxes'),
     dcc.Checklist(
@@ -19,5 +23,6 @@ app.layout = html.Div([
     )
 ])
 
+#App server
 if __name__ == '__main__':
     app.run_server(debug=True)
