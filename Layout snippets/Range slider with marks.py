e9b65a42e7dce3d0fe142f52f3f97be47be45bed
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 16:09:20 -0500

    Create Range slider with marks.py
    Range slider with marks snippet and comments added

--- Layout snippets/Range slider with marks.py
+++ Layout snippets/Range slider with marks.py
@@ -11,13 +11,13 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 #App layout, it describes what the applications looks like
-app.layout = html.Div([
-    html.Label('Text Input'),
-    dcc.Input(
-        placeholder='',
-        type='text',
-        value=''
-    )
+app.layout = html.Div(children=[
+    dcc.RangeSlider(
+        marks={i: 'Label {}'.format(i) for i in range(-5, 7)},
+        min=-5,
+        max=6,
+        value=[-3, 4]
+    ) 
 ])
 
 #App server
