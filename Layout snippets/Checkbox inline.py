7a83aec4f2e6f2478ce701da94a520713c9bb5d4
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 16:19:10 -0500

    Create Checkboc-inline.py
    Checkbox-inline snippet and rename Checkbox

---
+++ Layout snippets/Checkbox inline.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#Dash imports
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+#External css style sheet
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+#Main app
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#App layout, it describes what the applications looks like
+app.layout = html.Div([
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value=['MTL', 'SF'],
+        labelStyle={'display': 'inline-block'}
+    )
+])
+
+#App server
+if __name__ == '__main__':
+    app.run_server(debug=True)
