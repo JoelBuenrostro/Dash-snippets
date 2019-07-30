95bd1bcbc01809ce01bda65e172186d5e09aec91
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 14:52:09 -0500

    Create Dropdown.py
    Dropdown snippet

---
+++ Layout snippets/Radio items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Label('Radio Items'),
+    dcc.RadioItems(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
