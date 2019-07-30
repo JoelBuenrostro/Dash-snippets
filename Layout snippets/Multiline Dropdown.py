e1dab772dcb74e2f8cfdce2e1a3b99a359e0f10a
Joel Buenrostro <joelbuenrostro@outlook.com>
2019-07-30 15:53:02 -0500

    Create Multiline Dropdown.py
    Multiline dropdown snippet

---
+++ Layout snippets/Multiline Dropdown.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Label('Dropdown'),
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': u'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        multi=True,
+        value='MTL'
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
